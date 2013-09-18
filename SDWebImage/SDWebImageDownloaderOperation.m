84cfffaf92e9c0c25a21c927cb972ec524423e6a
Zhe Wu <wu@madk.org>
2013-09-18 16:12:38 +0800

    timeout too low for China

--- SDWebImage/SDWebImageDownloaderOperation.m
+++ SDWebImage/SDWebImageDownloaderOperation.m
@@ -73,7 +73,7 @@
         // Make sure to run the runloop in our background thread so it can process downloaded data
         // Note: we use a timeout to work around an issue with NSURLConnection cancel under iOS 5
         //       not waking up the runloop, leading to dead threads (see https://github.com/rs/SDWebImage/issues/466)
-        CFRunLoopRunInMode(kCFRunLoopDefaultMode, 10, false);
+        CFRunLoopRunInMode(kCFRunLoopDefaultMode, 30, false);
         if (!self.isFinished)
         {
             [self.connection cancel];
